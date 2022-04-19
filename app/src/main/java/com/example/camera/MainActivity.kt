83cc1e944f365a69e35e9d7b7a2fe912156d5d3a
uma <umamageswari.doodleblue@gmail.com>
2022-04-19 15:25:26 +0530

    camera start

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.camera
-
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
+import java.io.File
 
-
+private const val REQUEST_CODE = 100
+private lateinit var imageview: ImageView
 class MainActivity : AppCompatActivity() {
+    private lateinit var filePhoto:File
+    private  val FILE_NAME = "photo.jpg"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +29,8 @@ class MainActivity : AppCompatActivity() {
             builder.setTitle(R.string.dialogTitle)
             builder.setMessage(R.string.dialogMessage)
             builder.setPositiveButton("Open Camera") { dialogInterface, which ->
-                val intent = Intent(this, Camera::class.java)
-                startActivity(intent)
                 Toast.makeText(this, "Opening Camera", Toast.LENGTH_SHORT).show()
+                cam()
             }
             builder.setNegativeButton("Open Gallery") { dialogInterface, which ->
                 val intent = Intent(this, Gallery::class.java)
@@ -35,5 +44,24 @@ class MainActivity : AppCompatActivity() {
             alertDialog.setCancelable(false)
             alertDialog.show()
         }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode== REQUEST_CODE && resultCode == Activity.RESULT_OK&& data!= null){
+            imageview.setImageBitmap(data?.extras?.get("data") as Bitmap)
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+    private fun cam() {
+        val Camintent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        filePhoto = getPhotoFile(FILE_NAME)
+        val providerFile = FileProvider.getUriForFile(this,"com.example.camera.fileprovider", filePhoto)
+        Camintent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+        startActivityForResult(Camintent, REQUEST_CODE)
+
+    }
+    private fun getPhotoFile(fileName: String): File {
+        val directoryStorage = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", directoryStorage)
     }
 }
